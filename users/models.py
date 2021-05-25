74700fe86286af6aa547fec52fe405b049bfe549
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-25 21:49:31 +0530

    User model done

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(unique=True, max_length=255)
+    password = models.CharField(max_length=255)
+    username = None
+
+    # making username none and USERNAME_FIELD to 'email' to make login system use email as USERNAME field
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = []
