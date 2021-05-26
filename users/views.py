6135da0ec2909f0504b64ed6affaeaabcf89e6cb
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-26 11:21:41 +0530

    login and jwt sign implemented

--- users/views.py
+++ users/views.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
+from rest_framework import response
 from .serializer import UserSerializer
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from .models import User
+import jwt
+import datetime
 
 # Create your views here.
 
@@ -16,3 +21,27 @@ class RegisterView(APIView):
         serializer.save()
 
         return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        return Response({'success': True, 'token': token})
